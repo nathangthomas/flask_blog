cca4c56fc5fbb912f477d4330586e0deb01018e2
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-03-19 18:59:28 -0600

    Add temp seed data

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Nathan Thomas',
+        'title': 'Blog Post 1',
+        'content': 'Content of my very first post.',
+        'date_posted': 'March 18th, 2020'
+    },
+    {
+        'author': 'Camille Terry',
+        'title': 'Blog Post 2',
+        'content': 'Content of another post.',
+        'date_posted': 'March 19th, 2020'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return 'About Page'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
