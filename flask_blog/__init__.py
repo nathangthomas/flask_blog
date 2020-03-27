7e2295cb9d0ef2e2b0844a2ba22de7716e3de9b3
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-03-27 00:06:44 -0600

    Configure mailer and add environment variables

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,7 +1,10 @@
+import os
+from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -13,5 +16,14 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
+
 
 from flask_blog import routes
