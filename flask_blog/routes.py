7dd3d8dfb567e0874bfbde68d8c022aae8f1f04c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-03-27 15:31:44 -0600

    Update routes

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -173,7 +173,7 @@ def reset_request():
         return redirect(url_for('login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
 def reset_token(token):
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -181,11 +181,11 @@ def reset_token(token):
     if user is None:
         flash('That is an invalid or expired token.', 'warning')
         return redirect(url_for('reset_request'))
-        form = ResetPasswordForm()
-        if form.validate_on_submit():
-            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-            user.password = hashed_password
-            db.session.commit()
-            flash('Your password has been updated!', 'success')
-            return redirect(url_for('login'))
-        return render_template('reset_token.html', title='Reset Password', form=form)
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated!', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
